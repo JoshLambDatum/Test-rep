7752e7338f245c83f06b30c47663a1824f2edf7a
JoshLambDatum <DatumEng@datum.local>
2016-07-15 11:41:10 +0100

    testing
    change to .r

---
+++ FFT_calc.R
@@ -0,0 +1,19 @@
+#Copy data and run to create FFT
+
+sps = 100;
+
+data_table = read.delim("clipboard",sep = "\t", header = FALSE,na.strings = "NA", skipNul= TRUE); #transposed table in clipboard
+data = melt(data_table,na.rm = TRUE);
+
+samples = length(data[,2]);
+
+fft_data = fft(data[,2]);
+FFT_data_mag = sqrt(Re(fft_data)^2+Im(fft_data)^2)*2/(samples);
+axis_freq = seq(0,sps,sps/(samples));
+plot(axis_freq[2:(samples/2)],
+     FFT_data_mag[2:(samples/2)],
+     type="l",
+     xaxp = c(0, sps/2, 20),
+     main="FFT",
+     xlab="Frequency (Hz)", 
+     ylab="magnitude");
